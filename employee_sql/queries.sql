605fb3c2cf4b9150ace4812fbf57222ab590b2c8
BobbyRosenbleeth <robert.nalls.rosenbleeth@gmail.com>
2023-02-23 16:20:27 -0500

    Adding final version.

--- employee_sql/queries.sql
+++ employee_sql/queries.sql
@@ -3,7 +3,7 @@ SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees as e
 INNER JOIN salaries as s ON e.emp_no=s.emp_no; 
 
---**Question 2
+--Question 2
 SELECT first_name, last_name, hire_date
 FROM employees
 WHERE EXTRACT(YEAR FROM  hire_date)=1986;
