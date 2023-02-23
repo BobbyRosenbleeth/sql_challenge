0de3d5fd1b9862743a38a020e3029f2fce4b3987
BobbyRosenbleeth <robert.nalls.rosenbleeth@gmail.com>
2023-02-23 16:16:52 -0500

    Adding final version.

--- employee_sql/queries.sql
+++ employee_sql/queries.sql
@@ -3,13 +3,13 @@ SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees as e
 INNER JOIN salaries as s ON e.emp_no=s.emp_no; 
 
---Question 2
+--**Question 2
 SELECT first_name, last_name, hire_date
 FROM employees
-WHERE DATE_PART('Year',  hire_date)=1986;
+WHERE EXTRACT(YEAR FROM  hire_date)=1986;
 
 --Question 3
-SELECT d.dept_no, dep.dept_name, d.emp_no, e.last_name, e.first_name
+SELECT d.dept_no, dept.dept_name, d.emp_no, e.last_name, e.first_name
 FROM dept_manager as d
 INNER JOIN departments as dept ON d.dept_no=dept.dept_no
 INNER JOIN employees as e ON d.emp_no=e.emp_no;
@@ -23,22 +23,23 @@ INNER JOIN departments as d ON de.dept_no=d.dept_no;
 --Question 5
 SELECT first_name, last_name, sex
 FROM employees
-WHERE first_name = 'Hercules' and SUBSTRING(last_name, 1, 1) = 'B'
+WHERE first_name = 'Hercules' and SUBSTRING(last_name, 1, 1) = 'B';
 
 --Question 6
-SELECT e.emp_no, e.first_name, e.last_name
+SELECT e.emp_no, e.last_name, e.first_name
 FROM employees as e
-INNER JOIN dept_emp as d ON e.emp_no=d.emp_no; 
-WHERE d.dept_no = 'd007'
+INNER JOIN dept_emp as d ON e.emp_no=d.emp_no
+WHERE d.dept_no = 'd007';
 
 --Question 7
-SELECT e.emp_no, e.first_name, e.last_name, d.dept_name
+SELECT e.emp_no, e.last_name,e.first_name, d.dept_name
 FROM employees as e
-INNER JOIN dept_emp as de ON e.emp_no=de.emp_no;
+INNER JOIN dept_emp as de ON e.emp_no=de.emp_no
 INNER JOIN departments as d ON de.dept_no=d.dept_no 
 WHERE d.dept_name = 'Sales' or d.dept_name = 'Development';
 
 --Question 8
-SELECT count(last_name)
+SELECT last_name, count(*)
 FROM employees
-ORDER BY count(last_name) DESC;
+GROUP BY last_name
+ORDER BY count(*) DESC;
